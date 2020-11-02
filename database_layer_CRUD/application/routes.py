17c50b3adade0a1ab390c7b2842508cef7fc8fae
kirankalsi <kiran.1998@hotmail.co.uk>
2020-11-02 15:53:23 +0000

    database layer schema tutorial and exercise

---
+++ database_layer_CRUD/application/routes.py
@@ -0,0 +1,36 @@
+from application import app, db
+from application.models import Games
+
+@app.route('/add/<game_name>')
+def add(game_name):
+    new_game = Games(name=game_name)
+    db.session.add(new_game)
+    db.session.commit()
+    return "Added new game to database"
+
+@app.route('/read')
+def read():
+    all_games = Games.query.all()
+    games_string = ""
+    for game in all_games:
+        games_string += "<br>"+ game.name
+    return games_string
+
+@app.route('/update/<name>')
+def update(name):
+    first_game = Games.query.first()
+    first_game.name = name
+    db.session.commit()
+    return first_game.name
+
+@app.route('/delete')
+def delete():
+    game_to_delete = Games.query.first()
+    db.session.delete(game_to_delete)
+    db.session.commit()
+    return "Game has been deleted"
+
+@app.route('/count')
+def count():
+    number_of_games = Games.query.count()
+    return str(number_of_games)
