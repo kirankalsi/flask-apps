c2c0264a3b0c92544a024861ddaf18ca672b2243
kirankalsi <kiran.1998@hotmail.co.uk>
2020-11-03 16:12:30 +0000

    is_complete object default false

--- to_do_list/application/models.py
+++ to_do_list/application/models.py
@@ -3,4 +3,4 @@ from application import db
 class Tasks(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     task = db.Column(db.String(50))
-    is_complete = db.Column(db.Boolean)
+    is_complete = db.Column(db.Boolean, default = False)
