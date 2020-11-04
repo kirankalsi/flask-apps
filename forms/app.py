a470a1875415d7789b547af31eb7fc011213d4c2
kirankalsi <kiran.1998@hotmail.co.uk>
2020-11-04 11:00:32 +0000

    forms tutorial and exercise combined

---
+++ forms/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, DateField, DecimalField, SelectField, SubmitField
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'YOUR_SECRET_KEY'
+
+class BasicForm(FlaskForm):
+    first_name = StringField('First Name')
+    last_name = StringField('Last Name')
+    age = IntegerField('Age')
+    DOB = DateField()
+    weight = DecimalField()
+    gender = SelectField('Gender', choices=[('Male'), ('Female')])
+    submit = SubmitField('Add Account')
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
+def register():
+    error = ""
+    form = BasicForm()
+
+    if request.method == 'POST':
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+
+        if len(first_name) == 0 or len(last_name) == 0:
+            error = "Please supply both first and last name"
+        else:
+            return 'Thank you ' + str(first_name) + ' ' + str(last_name)
+
+    return render_template('home.html', form=form, message=error)
+
+if __name__ == '__main__':
+     app.run(debug=True, host='0.0.0.0')
